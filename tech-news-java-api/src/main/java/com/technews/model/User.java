8396c5b823432fb1921d749cd4cdf1c4489cf8c2
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-12-08 17:38:44 -0800

    Create Table Relationships for the User Class

--- tech-news-java-api/src/main/java/com/technews/model/User.java
+++ tech-news-java-api/src/main/java/com/technews/model/User.java
@@ -17,8 +17,12 @@ public class User{
     private String password;
     @Transient
     boolean loggedIn;
-
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Post> posts;
+    // Need to use FetchType.LAZY to resolve multiple bags exception
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<vote> votes;
+    // Need to use FetchType.LAZY to resolve multiple bags exception
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comment;
 }
